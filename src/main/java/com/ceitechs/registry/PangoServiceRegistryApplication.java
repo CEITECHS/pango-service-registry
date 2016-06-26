5c8f7447762a6562e93c416e84c21baa51634279
Iddy Magohe <iddyiam@gmail.com>
2016-06-25 20:24:56 -0400

    config server in pom

--- src/main/java/com/ceitechs/registry/PangoServiceRegistryApplication.java
+++ src/main/java/com/ceitechs/registry/PangoServiceRegistryApplication.java
@@ -2,7 +2,9 @@ package com.ceitechs.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class PangoServiceRegistryApplication {
 
