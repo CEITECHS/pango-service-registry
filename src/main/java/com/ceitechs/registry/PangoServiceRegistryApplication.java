5ddf3154b34d10c30ed810179a0a2eee0e304491
Iddy Magohe <iddyiam@gmail.com>
2016-06-25 20:09:56 -0400

    initial

---
+++ src/main/java/com/ceitechs/registry/PangoServiceRegistryApplication.java
@@ -0,0 +1,12 @@
+package com.ceitechs.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangoServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PangoServiceRegistryApplication.class, args);
+	}
+}
